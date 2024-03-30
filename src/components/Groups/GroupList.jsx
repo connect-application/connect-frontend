416aa3aad8fee70328b39c2e33beddc9c8b6f892
Sunvir <sunvirsingh72@gmail.com>
2024-03-30 19:30:55 -0400

    Cosmetic changes

--- src/components/Groups/GroupList.jsx
+++ src/components/Groups/GroupList.jsx
@@ -1,21 +1,29 @@
-const GroupList = ({ groups }) => {
-    return (
-        <div className="row row-cols-1 row-cols-md-3 g-4">
-            {groups.map((group) => (
-                <div className="col" key={group.groupId}>
-                    <div className="card h-100">
-                        <div className="card-body">
-                            <h5 className="card-title">{group.groupName}</h5>
-                            <p className="card-text">Category: {group.categoryId}</p>
-                            <p className="card-text">Code: {group.groupCode}</p>
-                            <p className="card-text">Owner: {group.groupOwner}</p>
-                        </div>
-                    </div>
-                </div>
-            ))}
-        </div>
 
-    );
+  
+const GroupList = ({ groups}) => {
+  
+
+  return (
+    
+    // <br></br><br></br>
+    <div className="row row-cols-1 row-cols-md-3 g-4">
+      {groups.map((group) => (
+        <div className="col" key={group.groupId}>
+          <div className="card border-info mb-3" style={{MaxWidth: "18rem"}}>
+            <div className="card-body" style={{padding: 0}}>
+            <div class="card-header">Category: {group.categoryId}</div>
+            <div class="card-body text-dark">
+              <h5 class="card-title">{group.groupName}</h5>
+              <p class="card-text">Owner: {group.groupOwner}</p>
+              <p class="card-text">Code: {group.groupCode}</p>
+            </div>
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+   
+  );
 };
 
 export default GroupList;
